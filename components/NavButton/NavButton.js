63c04e06b4aa475877005af9a04187ef2fe59b3e
Wladimir Schkulov <wladimir.schkulov1995@gmail.com>
2024-07-16 12:09:48 +0200

    Revert "Merge pull request #8 from E-LeeSchaffer/NavButtons"
    This reverts commit 1ce5ab6a41bc97af52a3269190b971be96d19973, reversing
    changes made to af34a9b7ccfab09e8723cb318e8938afe9b8325e.

--- components/NavButton/NavButton.js
+++
@@ -1,7 +0,0 @@
-export function NavButton(label, onClick) {
-  const button = document.createElement("button");
-  button.textContent = label;
-  button.className = "button";
-  button.addEventListener("click", onClick);
-  return button;
-}
--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
-
-import { NavButton } from "./components/NavButton/NavButton.js";
-import { NavPagination } from "./components/NavPagination/NavPagination.js";
-
 import { SearchBar } from "./components/SearchBar/SearchBar.js";
-
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const navigation = document.querySelector('[data-js="navigation"]');
-// const prevButton = document.querySelector('[data-js="button-prev"]');
-// const nextButton = document.querySelector('[data-js="button-next"]');
-const paginationContainer = document.querySelector('[data-js="pagination"]'); // const geändert.
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 const maxPage = 42;
@@ -38,30 +33,26 @@ async function fetchCharacters() {
       });
       cardContainer.appendChild(card);
     });
-    paginationContainer.textContent = `${page} / ${maxPage}`;
+
+    pagination.textContent = `${page} / ${maxPage}`;
   } catch (error) {
     console.error("Error fetching characters:", error);
   }
 }
-
-
-const prevClick = () => {
 
 prevButton.addEventListener("click", () => {
   if (page > 1) {
     page--;
     fetchCharacters();
   }
-};
+});
 
-const nextClick = () => {
+nextButton.addEventListener("click", () => {
   if (page < maxPage) {
     page++;
     fetchCharacters();
   }
-
-};
-
+});
 
 function handleSearchSubmit(event) {
   event.preventDefault();
@@ -70,18 +61,9 @@ function handleSearchSubmit(event) {
 
   page = 1;
   fetchCharacters();
-
-});
-
-const prevButton = NavButton("previous", prevClick);
-const nextButton = NavButton("next", nextClick);
-
-navigation.append(prevButton, paginationContainer, nextButton);
-
 }
 
 const searchBar = SearchBar(handleSearchSubmit);
 searchBarContainer.appendChild(searchBar);
-
 
 fetchCharacters();
