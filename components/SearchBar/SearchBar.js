570449edc9777d0b4f1d6a18b2ecfa930d90c38b
Eun-Rae Lee-Schaffer <eunraelee.erl@gmail.com>
2024-07-16 09:44:25 +0200

    delete searchbar form in html

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,12 +1,30 @@
-export function searchCharacterInSearchBar(searchQuery, fetchCharacters, page) {
-  const searchBar = document.querySelector('[data-js="search-bar"]');
-  searchBar.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(searchBar);
-    searchQuery.value = formData.get("query");
-    page = 1;
-    fetchCharacters();
-  });
-}
+// export function searchCharacterInSearchBar(searchQuery, fetchCharacters, page) {
+//   const searchBar = document.querySelector('[data-js="search-bar"]');
+//   searchBar.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(searchBar);
+//     searchQuery.value = formData.get("query");
+//     page = 1;
+//     fetchCharacters();
+//   });
+// }
 
-console.log(searchCharacterInSearchBar);
+// console.log(searchCharacterInSearchBar);
+
+// Aus html
+/* <form action="" class="search-bar" data-js="search-bar">
+          <input
+            name="query"
+            class="search-bar__input"
+            type="text"
+            placeholder="search characters"
+            aria-label="character name"
+          />
+          <button class="search-bar__button" aria-label="search for character">
+            <img
+              class="search-bar__icon"
+              src="assets/magnifying-glass.png"
+              alt=""
+            />
+          </button>
+        </form> */
